6182d762b8835a7fba0490914a728dc72e4bffbc
YUKESH CHANDRAN M <109645230+Yukesh-Chandran@users.noreply.github.com>
2023-01-30 22:03:47 +0530

    Create C program to print the word and to count the number of character in the word.c

---
+++ C program to print the word and to count the number of character in the word.c
@@ -0,0 +1,6 @@
+// C program to print the word and to count the number of character in the word
+#include<stdio.h>
+int main(){
+	printf("%d",printf("C programming\n")); //here space is also considered as character
+	return 0;
+}
