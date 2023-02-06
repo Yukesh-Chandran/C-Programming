ae8b650026e68fe3288f1c6799bd2ec7a0df4b71
YUKESH CHANDRAN M <109645230+Yukesh-Chandran@users.noreply.github.com>
2023-02-06 21:01:44 +0530

    Update Addition of two numbers.c

--- Addition of two numbers.c
+++ Addition of two numbers.c
@@ -2,9 +2,9 @@
 int main(){
   int num1,num2;
   printf("Enter the first number: ");
-  Scanf("%d",&num1);
+  scanf("%d",&num1);
   printf("Enter the second number");
-  Scanf("%d",&num2);
+  scanf("%d",&num2);
   printf("%d",num1+num2);
   return 0;
 }
