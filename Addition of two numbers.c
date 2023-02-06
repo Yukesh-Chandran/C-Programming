4ebd423148ffdd53fa1b7da8bf284493bb9b8d45
YUKESH CHANDRAN M <109645230+Yukesh-Chandran@users.noreply.github.com>
2023-02-06 20:59:57 +0530

    Create Addition of two numbers.c

---
+++ Addition of two numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  int num1,num2;
+  printf("Enter the first number: ");
+  Scanf("%d",&num1);
+  printf("Enter the second number");
+  Scanf("%d",&num2);
+  printf("%d",num1+num2);
+  return 0;
+}
+  
